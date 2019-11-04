5358b47e98af63d73a41a1028cb54d26c1720769
Jenny <jennserene@gmail.com>
2019-11-04 18:39:51 -0500

    Made science increment every time you press launch

--- script/game.js
+++ script/game.js
@@ -1,1 +1,12 @@
-var science
+var gameState = {
+    science: 0,
+    money: 0,
+};
+var inst = setInterval(updateGameState, 100);
+function updateGameState() {
+    let scienceNum = document.getElementById('scienceNum');
+    scienceNum.innerHTML = gameState.science;
+}
+function incrementScience() {
+    gameState.science++;
+}
