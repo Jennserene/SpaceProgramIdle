5712a8b5947c63f01fe03615f1ab0649d195a46e
Jenny <jennserene@gmail.com>
2019-11-04 20:06:39 -0500

    added comments to game.js

--- script/game.js
+++ script/game.js
@@ -1,12 +1,16 @@
+//Declare gameState object, this object contains all the main stats
 var gameState = {
     science: 0,
     money: 0,
 };
+//Run updateGameState 10 times a second
 var inst = setInterval(updateGameState, 100);
+//Every time this runs update the DOM to the current gameState
 function updateGameState() {
     let scienceNum = document.getElementById('scienceNum');
     scienceNum.innerHTML = gameState.science;
 }
+//On pressing Launch! button increment science by 1
 function incrementScience() {
     gameState.science++;
 }
