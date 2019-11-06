0e90ab2635b05b1a52f23503edd4273b05f9a11b
Jenny <jennserene@gmail.com>
2019-11-05 19:22:53 -0500

    addEventListener isn't working!

--- script/ui.js
+++ script/ui.js
@@ -1,13 +1,18 @@
+document.getElementById("researchMenuButton").addEventListener("click", test);
+function test() 
+{
+    window.alert("Research!");
+}
 function showTab(tabid)
 {
-    //get main element so that it's children can be accessed
+    // get main element so that it's children can be accessed
     let tabContainer = document.getElementById('tabContainer');
-    //put main element's children in an array
+    // put main element's children in an array
     let tabs = tabContainer.children;
     // cycles through all the tabs every time the function is called
     for (i = 0; i < tabs.length; i++)
     {
-        //if array with i index is the same as the button pressed then:
+        // if array with i index is the same as the button pressed then:
         if (tabs[i].id == tabid) {
             // add active class if not present
             tabs[i].classList.add('active');
@@ -19,14 +24,14 @@ function showTab(tabid)
 }
 function showBldg(bldgid)
 {
-    //get div with details that the button specifies
+    // get div with details that the button specifies
     let bldgContainer = document.getElementById(bldgid)
-    //if the div's class contains active then:
+    // if the div's class contains active then:
     if (bldgContainer.classList.contains('active')) {
-        //remove active class
+        // remove active class
         bldgContainer.classList.remove('active');
     } else {
-        //else, if the div's class doesn't contain active, add active class
+        // else, if the div's class doesn't contain active, add active class
         bldgContainer.classList.add('active');
     }
 }
