9e8ecf8b7cbc8629b906170adee298b308b42e8a
Jenny <jennserene@gmail.com>
2019-11-06 17:07:32 -0500

    changed updateGameState to run 20 times per second

--- script/game.js
+++ script/game.js
@@ -4,7 +4,7 @@ var gameState = {
     money: 0,
 };
 // Run updateGameState 10 times a second
-var inst = setInterval(updateGameState, 100);
+var inst = setInterval(updateGameState, 50);
 // Every time this runs update the DOM to the current gameState
 function updateGameState() {
     let scienceNum = document.getElementById('scienceNum');
