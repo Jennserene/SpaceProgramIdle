545f312dee115f3de63d65b089e2176a64146350
vtk <vidthekid@gmail.com>
2019-11-06 19:11:50 -0500

    make minor DOM changes without storing element references in variables

--- script/game.js
+++ script/game.js
@@ -7,10 +7,8 @@ var gameState = {
 var inst = setInterval(updateGameState, 100);
 // Every time this runs update the DOM to the current gameState
 function updateGameState() {
-    let scienceNum = document.getElementById('scienceNum');
-    let moneyNum = document.getElementById('moneyNum');
-    scienceNum.innerHTML = gameState.science;
-    moneyNum.innerHTML = gameState.money;
+    document.getElementById('scienceNum').innerHTML = gameState.science;
+    document.getElementById('moneyNum').innerHTML = gameState.money;
 }
 document.getElementById("launchButton").addEventListener("click", incrementScience);
 // On pressing Launch! button increment science by 1
